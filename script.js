77a1a5d536dcf886aeb34cef68ab57b9d3d598b0
deepak lakwal <dkumar17071@gmail.com>
2024-12-05 20:42:34 +0530

    adding score functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let score = {
+  wins: 0,
+  loses: 0,
+  ties: 0,
+};
+
 function playGame(userChoice) {
   const choices = ["Rock", "Paper", "Scissors"];
   const computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -5,14 +11,17 @@ function playGame(userChoice) {
   let winner = "";
   if (userChoice === computerChoice) {
     winner = "It's a Tie";
+    score.ties++;
   } else if (
     (userChoice === "Rock" && computerChoice === "Scissors") ||
     (userChoice === "Paper" && computerChoice === "Rock") ||
     (userChoice === "Scissors" && computerChoice === "Paper")
   ) {
     winner = "You Won";
+    score.wins++;
   } else {
     winner = "Computer Won";
+    score.loses++;
   }
 
   document.getElementById(
@@ -24,4 +33,14 @@ function playGame(userChoice) {
   document.getElementById(
     "winner"
   ).innerHTML = `Winner is : <span>${winner} </span>`;
+
+  document.getElementById(
+    "wins"
+  ).innerHTML = `Your Wins : <span>${score.wins} </span>`;
+  document.getElementById(
+    "loses"
+  ).innerHTML = `Computers wins : <span>${score.loses} </span>`;
+  document.getElementById(
+    "ties"
+  ).innerHTML = `Ties: <span>${score.ties} </span>`;
 }
